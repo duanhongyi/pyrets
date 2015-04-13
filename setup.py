cb7e4b683c133f4b9fb224bb35f04c94ad6a3a97
duanhongyi <duanhongyi@duanhongyis-Mac-Pro.local>
2015-04-13 13:39:34 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+
+setup(name='scseg',
+      version='1.7.4',
+      description='a python rets client',
+      long_description=README
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      author='duanhongyi',
+      author_email='duanhyi@gmail.com',
+      url='https://github.com/duanhongyi/pyrets',
+      keywords='Python rets client',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[],
+      platforms = 'all platform',
+      license = 'BSD',
+      )
